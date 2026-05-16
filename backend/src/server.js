a052cdc08d3d9245a542f1b04a00e432b47a956b
PiyushVala2208 <piyushvala45@gmail.com>
2026-05-16 12:04:33 +0530

    welsome email implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,17 @@
 import express from "express";
-import dotenv from "dotenv";
 import path from "path";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
-dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 8000;
+const PORT = ENV.PORT || 8000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -21,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 //make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get(/^(?!\/api).+/, (_, res) => {
