6babae328ed87c75271429243dd1259dcd40eccd
PiyushVala2208 <piyushvala45@gmail.com>
2026-05-22 17:09:13 +0530

    message routes optimized

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -15,7 +15,7 @@ const messageSchema = new mongoose.Schema(
     text: {
       type: String,
       trim: true,
-      maxlength: 2000,
+      maxlength: 2208,
     },
     image: {
       type: String,
